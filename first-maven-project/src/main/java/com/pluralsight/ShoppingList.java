40b0ef7bb3c17eebc9cda6541ee82bba9a454c21
Gai'Nicholas Patton <nickpatton1999@yahoo.com>
2025-04-10 10:30:45 -0400

    completed ShoppingList app

---
+++ first-maven-project/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,9 @@
+package com.pluralsight;
+
+public class ShoppingList {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Cheese, Bread, Eggs, Butter, Milk, Juice, Chicken, Steak, Chips, Water");
+    }
+}
